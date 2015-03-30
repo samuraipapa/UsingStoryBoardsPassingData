53353a6176b081747be974cfc2788fe6c948638a
samuraipapa <yury.gitman@gmail.com>
2015-03-30 14:39:20 -0400

    StoryBoard Set-UP,  Now it Needs Code

---
+++ Photo.swift
@@ -0,0 +1,18 @@
+//
+//  Photo.swift
+//  UsingStoryBoardsPassingData
+//
+//  Created by GrownYoda on 3/30/15.
+//  Copyright (c) 2015 yuryg. All rights reserved.
+//
+//  Code started from http://www.lynda.com/Swift-tutorials/Step-two-Creating-custom-classes/185036/362323-4.html
+
+import Foundation
+
+
+class Photo: NSObject {
+    
+    var name: String?
+    var fileName : String?
+    var notes: String?
+}
